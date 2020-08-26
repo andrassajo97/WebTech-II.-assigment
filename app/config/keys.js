a43db148a2e87423786993b8332ba97ab8536d81
Sajó András <43603768+andrassajo97@users.noreply.github.com>
2020-08-26 17:11:09 +0200

    Update keys.js

--- app/config/keys.js
+++ app/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://andrassajo97:Bandi20203A@handball-base-4fn9m.mongodb.net/test?retryWrites=true&w=majority",
+    "",
   secretOrKey: "secret",
 };
